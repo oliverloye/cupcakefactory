48882f14e2bee7587ecb03f88a73653332b57584
Oliver <oliverloye@me.com>
2017-02-28 16:03:13 +0100

    -

--- src/java/data/RegControler.java
+++ src/java/data/RegControler.java
@@ -11,6 +11,8 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -18,13 +20,14 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Oliver
  */
-public class RegControler {
+@WebServlet(name = "RegControler", urlPatterns = {"/RegControler"})
+public class RegControler extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        CustomerMapper cm = new CustomerMapper();
+        
         try {
-            CustomerMapper cm = new CustomerMapper();
-            
             
             response.setContentType("text/html; charset=UTF-8");
             
@@ -37,18 +40,29 @@ public class RegControler {
             //Indsætte oplysniger i databasen
             cm.setCustomer(username, password, cname, email, 100);
             
-            //Set data
-            
-            request.getSession().setAttribute("cname", cname);
-            request.getSession().setAttribute("username", username);
-            request.getSession().setAttribute("password", password);
-            request.getSession().setAttribute("email", email);
-            
             //Sender data
             request.getRequestDispatcher("view.jsp").forward(request, response);
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
+    }
+        
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                    throws ServletException, IOException {
+            processRequest(request, response);
+        }
+
+        @Override
+        protected void doPost(HttpServletRequest request, HttpServletResponse response)
+                throws ServletException, IOException {
+            processRequest(request, response);
+        }
+
+        @Override
+        public String getServletInfo() {
+            return "Short description";
+        }
         
     }
-}
+
